016ed473689c21b198246ec274683c4b50a9c92a
cyboranf <cyboranf@gmail.com>
2022-10-19 14:35:33 +0200

    SpringBoot configured and maven dependency added

---
+++ src/main/java/com/example/StringFreestyleApi/StringFreestyleApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.StringFreestyleApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringFreestyleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringFreestyleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/StringFreestyleApi/StringFreestyleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StringFreestyleApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringFreestyleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
