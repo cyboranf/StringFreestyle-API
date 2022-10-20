4ea632d605b8cae7494ee3a1688b75d98eb7c93f
cyboranf <cyboranf@gmail.com>
2022-10-20 22:01:56 +0200

    create and write to file method -added

--- src/main/java/com/example/StringFreestyleApi/web/StringCreatorController.java
+++ src/main/java/com/example/StringFreestyleApi/web/StringCreatorController.java
@@ -1,11 +1,10 @@
 package com.example.StringFreestyleApi.web;
 
 import com.example.StringFreestyleApi.domain.Sign;
-import com.example.StringFreestyleApi.domain.User;
 import com.example.StringFreestyleApi.service.SignService;
-import com.example.StringFreestyleApi.service.UserService;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -14,18 +13,22 @@ import java.util.List;
 @RequestMapping("/generate")
 public class StringCreatorController {
     private long id = 1;
-    private final UserService userService;
+
     private final SignService signService;
 
-    public StringCreatorController(UserService userService, SignService signService) {
-        this.userService = userService;
+    public StringCreatorController(SignService signService) {
+
         this.signService = signService;
     }
 
     @PostMapping("/{min}/{max}/{elements}/{wanted}")
-    public String defineGenerate(@PathVariable long min, @PathVariable long max, @PathVariable String elements, @PathVariable long wanted) {
-        User user = new User();
+    public String defineWhatGenerate(@PathVariable long min, @PathVariable long max, @PathVariable String elements, @PathVariable long wanted) {
         Sign sign = new Sign();
+        List<Sign> signList = signService.findAll();
+        for (Sign sign1 : signList) {
+            id++;
+        }
+
         sign.setId(id);
         sign.setMinSizeString(min);
         sign.setMaxSizeString(max);
@@ -34,14 +37,19 @@ public class StringCreatorController {
 
 
         sign.setMaxQuantity(maxPossibilities(elements, min, max));
-        maxQuantityTest = 0;
+        if (sign.getMaxQuantity() >= wanted) {
+            sign.setPossible(true);
+        } else {
+            sign.setPossible(false);
+        }
+
         signService.saveSign(sign);
-        id++;
+
         return sign.toString();
     }
 
     @GetMapping("/{id}")
-    public List<String> generateStrings(@PathVariable long id) {
+    public List<String> generateStrings(@PathVariable long id) throws IOException {
         generatedWordsList.clear();
 
         Sign signParams = signService.findById(id);
@@ -50,39 +58,68 @@ public class StringCreatorController {
         String elements = signParams.getElementsOfString();
         long wantedQuantity = signParams.getWantQuantity();
         long maxQuantity = signParams.getMaxQuantity();
+        boolean isPossible = signParams.isPossible();
 
-        long minAlso = min;
 
-        if (maxQuantity >= wantedQuantity) {
-            generatedWordsList.add("You wanted to build "+wantedQuantity+" strings with minimum length = "+min+" and maximum = "+max+" made by chars from: "+elements);
+        if (isPossible) {
+            generatedWordsList.add("You wanted to build " + wantedQuantity + " strings with minimum length = " + min + " and maximum = " + max + " made by chars from: " + elements);
             changeGeneratedToList(elements, min, max, wantedQuantity);
+            createAndWriteToFile(generatedWordsList, isPossible, min, max, elements, wantedQuantity);
         } else {
-            generatedWordsList.add("You wanted to build "+wantedQuantity+" strings with minimum length = "+min+" and maximum = "+max+" made by chars from: "+elements);
+            generatedWordsList.add("You wanted to build " + wantedQuantity + " strings with minimum length = " + min + " and maximum = " + max + " made by chars from: " + elements);
             generatedWordsList.add("Error: u cant build: " + wantedQuantity + " strings, because " + maxQuantity + " is max");
         }
         return generatedWordsList;
 
     }
 
+    @GetMapping("/{min}/{max}/{elements}/{wanted}")
+    public List<String> wordsGeneratorWAnotherLink(@PathVariable long min, @PathVariable long max, @PathVariable String elements, @PathVariable long wanted) throws IOException {
+        Sign sign = new Sign();
+        List<Sign> signList = signService.findAll();
+        for (Sign sign1 : signList) {
+            id++;
+        }
+
+        sign.setId(id);
+        sign.setMinSizeString(min);
+        sign.setMaxSizeString(max);
+        sign.setElementsOfString(elements);
+        sign.setWantQuantity(wanted);
+
+
+        sign.setMaxQuantity(maxPossibilities(elements, min, max));
+        if (sign.getMaxQuantity() >= wanted) {
+            sign.setPossible(true);
+        } else {
+            sign.setPossible(false);
+        }
+        signService.saveSign(sign);
+
+        if (sign.isPossible()) {
+            generatedWordsList.add("You wanted to build " + wanted + " strings with minimum length = " + min + " and maximum = " + max + " made by chars from: " + elements);
+            changeGeneratedToList(elements, min, max, wanted);
+            createAndWriteToFile(generatedWordsList, sign.isPossible(), min, max, elements, wanted);
+        } else {
+            generatedWordsList.add("You wanted to build " + wanted + " strings with minimum length = " + min + " and maximum = " + max + " made by chars from: " + elements);
+            generatedWordsList.add("Error: u cant build: " + wanted + " strings, because " + maxQuantity + " is max");
+        }
+        return generatedWordsList;
+    }
+
     private List<String> generatedWordsList = new ArrayList<>();
     private long maxQuantity = 0;
-    private long maxQuantityTest = 0;
+
 
     void generateStrings(String str, char[] data,
                          long last, int index, long rowLimit) {
         int length = str.length();
-
-
         for (int i = 0; i < length; i++) {
-
             data[index] = str.charAt(i);
-
             if (index == last) {
                 if (rowLimit == maxQuantity) {
                     return;
                 }
-                ;
-                //System.out.println(new String(data));
                 generatedWordsList.add(new String(data));
                 maxQuantity++;
             } else
@@ -96,14 +133,11 @@ public class StringCreatorController {
         if (rowLimit == maxQuantity) {
             return;
         }
-        ;
         char[] data = new char[(int) length];
         char[] temp = str.toCharArray();
 
         Arrays.sort(temp);
         str = new String(temp);
-        List<String> strList = new ArrayList<>();
-
         generateStrings(str, data, length - 1, 0, rowLimit);
     }
 
@@ -122,6 +156,24 @@ public class StringCreatorController {
             mip = mip + Math.pow(str.length(), i);
         }
         return (long) mip;
+    }
+
+    private void createAndWriteToFile(List<String> stringList, boolean possible, long min, long max, String elements, long wantedQuantity) throws IOException {
+        String nameFile = "generate" + min + max + elements + wantedQuantity + ".txt";
+        if (possible) {
+            File file = new File(nameFile);
+            if (!file.exists()) {
+                file.createNewFile();
+
+            }
+
+            PrintWriter printWriter = new PrintWriter(file);
+            for (String permutation : stringList) {
+                printWriter.println(permutation);
+            }
+            printWriter.close();
+        }
+
     }
 
 }
